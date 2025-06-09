374a65c53fe6599b7dac1ce9720b1058af9c3650
priyanathbhukta <priyanathbhukta@gmail.com>
2025-06-09 17:23:10 +0530

    initial commit

---
+++ src/test/java/com/priyanathbhukta/notenest/NoteNestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanathbhukta.notenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteNestApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
