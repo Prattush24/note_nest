dc46f1dd909be3b9a1297171fedc0055f0f34a24
Priyanath Bhukta <priyanathbhukta@gmail.com>
2025-06-18 10:46:37 +0530

    exception implementation

---
+++ src/main/java/com/priyanathbhukta/notenest/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.priyanathbhukta.notenest.exception;
+
+public class ResourceNotFoundException extends Exception{
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+	
+}
